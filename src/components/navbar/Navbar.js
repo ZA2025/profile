99f5581e72ac53c5ea90ce1e7e99eb6d1d98d73a
ZA2025 <zainabal2025@gmail.com>
2025-04-22 10:33:40 +0100

    sass fixes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -24,9 +24,7 @@ const Navbar = () => {
             </div>
              
             <ul className={`${styles.dropdown} ${isMenuOpen ? styles.open : ''}`}>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="/">Home</a></li>
             </ul>
         </nav>
     );
